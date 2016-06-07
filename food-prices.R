3da1bc34e677e452135b92d1472a0e39f6518482
Eugene Sovetkin <e.sovetkin@gmail.com>
2016-06-07 14:55:49 +0200

    moved to function

--- food-prices.R
+++ food-prices.R
@@ -8,88 +8,10 @@ food <- read.ledger(paste("food house bath other -f ~/bank/food-ledger_2013.log 
                           "-f ~/bank/food-ledger_2014.log -f ~/bank/food-ledger_2015.log ",
                           "-f ~/bank/food-ledger.log -H -X ",currency," -b 2016-03-15"))
 
-food$Description <- tolower(food$Description)
 
-## paying currency and amount
-Currency <- as.character(food$Currency)
-Amount <- as.numeric(food$Amount)
+food <- food.prices.convert(food,currency)
 
-## Price vector
-Price <- rep(NA,nrow(food))
-Price.currn <- rep(NA,nrow(food))
-
-## Convert comments to character
-Notes <- as.character(food$Note)
-
-## prices like "0.5kg @ 1.99 EUR" or "10x @ 100g"
-regstr <- "([.0-9]+)[ ]?([[:alpha:]]+)[ ]?@[ ]?([.0-9]+)[ ]?([[:alpha:]]+)"
-str <- regmatches(Notes,gregexpr(regstr,Notes))
-idx.at <- rep(1:nrow(food),sapply(str,length))
-str <- unlist(str)
-
-at.value <- as.numeric(gsub(regstr,"\\3",str))
-at.currn <- gsub(regstr,"\\4",str)
-
-unit.value <- as.numeric(gsub(regstr,"\\1",str))
-unit.currn <- gsub(regstr,"\\2",str)
-
-## index of recorded prices, i.e. currency coincide
-idx.rec <- at.currn == Currency[idx.at]
-
-Price.currn[idx.at][idx.rec] <-
-    paste("\"",at.currn[idx.rec],"/",unit.currn[idx.rec],"\"",sep="")
-Price[idx.at][idx.rec] <- at.value[idx.rec]
-
-## for others the following rule applies
-Price[idx.at][!idx.rec] <- Amount[idx.at][!idx.rec] / (at.value[!idx.rec]*unit.value[!idx.rec])
-Price.currn[idx.at][!idx.rec] <- 
-    paste("\"",Currency[idx.at][!idx.rec],"/",at.currn[!idx.rec],"\"",sep="")
-
-## remove captured items
-Notes <- gsub(regstr,"",Notes)
-
-## prices like " @ 0.99 EUR/kg"
-regstr <- "@[ ]?([.0-9]+)[ ]?([[:alpha:]]+/[[:alpha:]]+)"
-m.at <- regexpr(regstr,Notes)
-idx.at <- m.at != -1
-str <- regmatches(Notes,m.at)
-
-Price[idx.at] <- as.numeric(gsub(regstr,"\\1",str))
-Price.currn[idx.at] <- paste("\"",gsub(regstr,"\\2",str),"\"",sep="")
-
-
-## remove captured items
-Notes <- gsub(regstr,"",Notes)
-
-## deal with other amounts
-regstr <- "([.0-9]+)[ ]?([[:alpha:]]+)"
-m.at <- regexpr(regstr,Notes)
-idx.at <- m.at != -1
-str <- regmatches(Notes,m.at)
-str
-
-unit.value <- as.numeric(gsub(regstr,"\\1",str))
-unit.currn <- gsub(regstr,"\\2",str)
-
-Price[idx.at] <- Amount[idx.at]/as.numeric(unit.value)
-Price.currn[idx.at] <-
-    paste("\"",Currency[idx.at],"/",unit.currn,"\"",sep="")
-
-## remove capture items. So far I have covered all cases above
-Notes <- gsub(regstr,"",Notes)
-
-## write data
-food$Currency <- Price.currn
-food$Amount <- Price
-
-food <- food[!is.na(food$Amount),]
-
-food[food$Currency == paste("\"",currency,"/g\"",sep=""),"Amount"] <-
-    food[food$Currency == paste("\"",currency,"/g\"",sep=""),"Amount"]*1000
-food[food$Currency == paste("\"",currency,"/g\"",sep=""),"Currency"] <-
-    paste("\"",currency,"/kg\"",sep="")
-
-
+#### evaluate some statistics on prices
 prices <- aggregate(food$Amount,by=list(food$Category,food$Description,food$Currency),
                     function(x) c("min"=min(x),"mean"=mean(x),"max"=max(x)))
 prices$min <- prices$x[,1]
@@ -112,4 +34,4 @@ print(xtable(data,digits=5),type="latex",file="food-prices.tex",
       hline.after = hline.after)
 write("\\end{document}",file="food-prices.tex",append=TRUE)
 
-system("pdflatex food-prices.tex && pdflatex food-prices.tex && mupdf food-prices.pdf &")
+system("pdflatex food-prices.tex && pdflatex food-prices.tex")
