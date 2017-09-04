c76ccfcb38d12f676d24b0ffe96502c4f44aea40
Jenya Sovetkin <e.sovetkin@gmail.com>
2017-09-04 14:36:18 +0200

    weekly and weekly.price functions

--- ledgerplots/R/ledger-functions.R
+++ ledgerplots/R/ledger-functions.R
@@ -288,6 +288,15 @@ monthly <- function(x) {
   filter(x,rep(1,30),sides=1)
 }
 
+#' @title Calculate 7 days sum of the fiven vector
+#'
+#' @param x a numeric vector
+#'
+#' @export
+weekly <- function(x) {
+  filter(x,rep(1,7),sides=1)
+}
+
 #' @title Monthly average price
 #'
 #' @description calculate monthly average price
@@ -299,6 +308,17 @@ monthly.price <- function(x) {
   n <- filter(abs(x) > 0,rep(1,30),sides=1)
   n[! abs(n) > 0] <- 1
   filter(x,rep(1,30),sides=1)/n
+}
+
+#' @title Calculate 7 days average price
+#'
+#' @param x a numeric vector
+#'
+#' @export
+weekly.price <- function(x) {
+  n <- filter(abs(x) > 0,rep(1,7),sides=1)
+  n[! abs(n) > 0] <- 1
+  filter(x,rep(1,7),sides=1)
 }
 
 #' @title duplicate entries in case there are multiple matches
