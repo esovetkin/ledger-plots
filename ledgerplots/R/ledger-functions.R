c040f0cbb532dc9fcdf0cbf0a8d02dcc28f507cd
Jenya Sovetkin <e.sovetkin@gmail.com>
2017-09-04 12:17:00 +0200

    function to generate price tables

--- ledgerplots/R/ledger-functions.R
+++ ledgerplots/R/ledger-functions.R
@@ -424,6 +424,66 @@ compound.poisson <- function(data, period = 365) {
   c(lambda.t*mean(data[,2]), sqrt(lambda.t*mean(data[,2]^2)))
 }
 
+#' Generate a latex table with food prices
+#'
+#' @param ofile output filename
+#'
+#' @param query ledger query
+#'
+#' @param ledger.options extra options passed to ledger
+#'
+#' @param ledger.path path to the ledger binary
+#'
+#' @export
+generate.price.table <- function(query, ofile="food-prices.text",
+                                 ledger.options, ledger.path=NULL) {
+  # read transactions
+  cat(paste("Reading transactions for the query:",query,ledger.options,"\n"))
+  data <- read.ledger(query = query, options = ledger.options,
+                      ledger.path = ledger.path)
+
+  # experiments
+  data <- read.ledger(
+    paste("food house bath other -f ~/bank/food-ledger_2013.log ",
+          "-f ~/bank/food-ledger_2014.log -f ~/bank/food-ledger_2015.log ",
+          "-f ~/bank/food-ledger_2016.log ",
+          "-f ~/bank/food-ledger.log -H -X EUR -b 2016-03-12"))
+
+  # parse transaction notes
+  data <- parse.notes(data)
+
+  # evaluate some statistics on prices
+  prices <- aggregate(data$Price,by=list(data$Category,data$Description,data$Price.curr),
+                      function(x) c("min"=min(x),"mean"=mean(x),"last"=tail(x,1)))
+
+  # rename column names
+  prices$min <- prices$x[,1]
+  prices$mean <- prices$x[,2]
+  prices$last <- prices$x[,3]
+  prices$x <- NULL
+  colnames(prices) <- c("category","shop","currency","min","mean","last")
+
+  # order by category and currency
+  data <- prices[grep("*",prices[,1],ignore.case = TRUE),]
+  data <- data[order(data$category,data$min,data$currency),]
+
+  # positions of the horizontal lines
+  hline.after <- c(-1,0,which(diff(as.numeric(data$category)) > 0),nrow(data))
+
+  require(xtable, quietly = TRUE)
+
+  # write table to the latex file
+  write(paste("\\documentclass[a4paper]{article} \\pagestyle{empty}",
+              " \\usepackage[utf8]{inputenc} \\usepackage{longtable}",
+              " \\usepackage[left=1cm, right=1cm, bottom=1cm,top=1cm]{geometry}",
+              "\\begin{document}",file=ofile))
+  print(xtable(data,digits=5),type="latex",file=ofile,
+        append=TRUE,tabular.environment = 'longtable',
+        include.rownames=FALSE,floating=FALSE,
+        hline.after = hline.after)
+  write("\\end{document}",file=ofile,append=TRUE)
+}
+
 #' Convert comments in food ledger to the corresponding prices
 #' @param food dataset with food, returns by read.ledger
 #' @param currency sometimes currency in transaction note is given
