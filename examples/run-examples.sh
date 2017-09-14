b0ecd4a81dfb38b73fb8895ec2dbbc5a3a54d0a3
Jenya Sovetkin <e.sovetkin@gmail.com>
2017-09-14 16:17:11 +0200

    updated examples

--- examples/run-examples.sh
+++ examples/run-examples.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-../ledger-plots -q "assets ;; income" -f "cumsum;; function(x) cumsum(-x)" --ledger-options='-f assets.ledger' -o assets.pdf
+../ledger-plots -q "assets ;; income" -f "cumsum :: function(x) {i<-1:length(x); predict(lm(cumsum(x)~i))} ;; function(x) cumsum(-x)" --ledger-options='-f assets.ledger' -o assets.pdf
 
-../ledger-plots -q "food bath" -f "monthly" --ledger-options='-f food.ledger' -o food.pdf
+../ledger-plots -q "food bath" -f "weekly :: function(x) {monthly(x)/4}" --ledger-options='-f food.ledger' -o food.pdf
 
 ../ledger-plots -q "food bath" -f "monthly.price" --ledger-options='-f food.ledger -H -X EUR' --type "price" -o food-prices.pdf
 
